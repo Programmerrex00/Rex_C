09c1a3c6d3f5056ad98563c97b4aa874c053c8ca
rexmax <maicolcubidespe@gmail.com>
2023-04-14 14:59:19 -0500

    Enviando mi codigo al repositorio

---
+++ proyect.c
@@ -0,0 +1,41 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main(){
+    
+ /*int horasTrabajadas;
+    float costoHora;
+    float sueldo;
+    printf("Ingrese las horas trabajadas por el empleado: ");
+    scanf("%i", &horasTrabajadas);
+    printf("Ingrese el pago por hora ");
+    scanf("%f", &costoHora);
+    sueldo = horasTrabajadas * costoHora;
+    printf("El sueldo total de la operacion es");
+    printf("%f", sueldo);
+    getchar();
+    return 0;*/
+
+   /*int lado;
+   int superficie;  
+   printf("Ingrese el lado del cuadrado");
+   scanf("%i", &lado);  
+   superficie = lado * lado;
+   printf("La superficie del cuadrado es de:\n");
+   printf("%i", superficie);
+    getchar();
+    return 0;*/
+
+    /*int numero1;
+    int numero2;
+    printf("Ingrese el primer numero positivo");
+    scanf("%i", &numero1);
+    printf("Ingrese el segundo numero segundo");
+    scanf("%i", &numero2);
+    printf("La suma de los dos numeros es de %i y la multiplicacion de los dos numero es de %i", (numero1+numero2), (numero1*numero2));
+    getchar();
+    return 0;*/
+
+    
+
+}
