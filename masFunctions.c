edf84e16a5d65b606a8713520e235b9d84a649f2
SALA ESTUDIANTES 16 <aula16@salasuts.uts.edu.co>
2023-05-09 14:50:36 -0500

    Ejercicios en la U otra vez

--- masFunctions.c
+++ masFunctions.c
@@ -91,7 +91,7 @@ void impresion(int vector[7]){
 
 //-----------------------------------------------------------------
 
-void recopilarPalabra(char palabra[40]){
+/*void recopilarPalabra(char palabra[40]){
     printf("Ingrese la palabra");
     gets(palabra);
     printf("\n");
@@ -100,14 +100,99 @@ void recopilarPalabra(char palabra[40]){
 int recopilarVocales(char palabra[40]){
     int x = 0, contador = 0;
     while (palabra[x] != '\0'){
-        if(){
-            
+        if(palabra[x] == 'a' || palabra[x] == 'e' && palabra[x] == 'i'
+        || palabra[x] == 'o' || palabra[x] == 'u' ||
+        palabra[x] == 'A' || palabra[x] == 'E' && palabra[x] == 'I'
+        || palabra[x] == 'O' || palabra[x] == 'U'){
+            contador++;
         }
         x++;
     }
     return contador;
+}*/
+
+//---------------------------------------------------------------
+
+
+/*float calcularPromedio(float alturas[5]){
+    float promedio = 0;
+    for (int i = 0; i < 5; i++){
+        promedio += alturas[i];
+    }
+    return promedio/5;
 }
 
+void alturasBajasYAltas(float alturas[5], float promedio){
+    int alturasBajas = 0, alturasAltas = 0;
+    for (int i = 0; i < 5; i++){
+        if(alturas[i] > promedio){
+            alturasAltas++;
+        }else{
+            alturasBajas++;
+        }
+    }
+    printf("\n La cantidad de alturas mayores al promedio es de: %i", alturasAltas);
+    printf("\n La cantidad de alturas menores al promedio es de: %i", alturasBajas);
+}*/
+
+//-------------------------------------------------------------------
+/*void cargarSueldos(float sueldos[4]){
+    for (int i = 0; i < 4; i++){
+        printf("Ingrese el sueldo del %i empleado: ", i+1);
+        scanf("%f", &sueldos[i]);
+        printf("\n");
+    }
+}
+
+float totalSueldos(float sueldos[4]){
+    float totalSueldosEmpleados = 0;
+    for (int i = 0; i < 4; i++){
+        totalSueldosEmpleados += sueldos[i];
+    }
+    return totalSueldosEmpleados;
+}*/
+//--------------------------------------------------------------------------
+
+/*void cargarDatos(int numeros[8]){
+    for (int i = 0; i < 8; i++){
+        printf("Ingrese el valor del indice %i: ", i+1);
+        scanf("%i", &numeros[i]);
+        printf("\n");
+    }
+}
+
+int valorAcumulado(int numeros[8]){
+    int valorAcumuladoNumeros = 0;
+    for (int i = 0; i < 8; i++){
+        valorAcumuladoNumeros += numeros[i];
+    }
+    return valorAcumuladoNumeros;
+}
+
+
+int valorAcumuladoMayoresA36(int numeros[8]){
+    int valorAcumuladoNumeros36 = 0;
+    for (int i = 0; i < 8; i++){
+        if(numeros[i] > 36){
+            valorAcumuladoNumeros36 += numeros[i];
+        }
+    }
+    return valorAcumuladoNumeros36;
+}
+
+
+int componentesMayoresA50(int numeros[8]){
+    int cantidad = 0;
+    for (int i = 0; i < 8; i++){
+        if(numeros[i] > 50){
+            cantidad++;
+        }
+    }
+    return cantidad;
+}*/
+
+
+//------------------------------------------------------------------------
 
 int main(){
     /*float largo, ancho;
@@ -142,13 +227,31 @@ int main(){
     printf("\n");
     printf("Los gatos totales entre los sueldos es de: %0.2f ", gatosTotales(sueldos));*/
 
-    char palabra[40];
-    recopilarPalabra(palabra);
-    
+//-------------------------------------------------------------------------------------
 
+    /*float alturaPersonas[5];
+    for (int i = 0; i < 5; i++){
+        printf("Ingrese la %i altura: ", i+1);
+        scanf("%f", &alturaPersonas[i]);
+        printf("\n");
+    }
+    printf("El promedio de las alturas de las personas es de: %0.2f",calcularPromedio(alturaPersonas));  
+    alturasBajasYAltas(alturaPersonas, calcularPromedio(alturaPersonas));*/
 
-    
-
-
+    //---------------------------------------------------------------------------------------------------------
+    /*float sueldosManana[4];
+    float sueldosTarde[4];
+    printf("Sueldos de Manana: \n");
+    cargarSueldos(sueldosManana);
+    printf("Sueldos de Tarde: \n");
+    cargarSueldos(sueldosTarde);
+    printf("El total de sueldos de la Manana es de: %0.2f\n", totalSueldos(sueldosManana));
+    printf("El total de sueldos de la Tarde es de: %0.2f", totalSueldos(sueldosTarde));*/
+    //--------------------------------------------------------------------------------------------
+    /*int numeros[8];
+    cargarDatos(numeros);
+    printf("El valor acumulado de la Lista es de: %i\n", valorAcumulado(numeros));
+    printf("El valor acumulado de los numeros mayores a 36 es de: %i\n", valorAcumuladoMayoresA36(numeros));
+    printf("La cantidad de numeros mayores a 50 es de: %i", componentesMayoresA50(numeros));*/
     
 }
