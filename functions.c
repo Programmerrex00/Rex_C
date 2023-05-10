3d3f9cbc9f123ccdd3d8ea8152810ad4dcb484d7
rexmax <maicolcubidespe@gmail.com>
2023-05-10 14:11:58 -0500

    rex

---
+++ functions.c
@@ -0,0 +1,69 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+//programacion estruturada
+
+
+
+//Funciones agotadas
+/*void presentacion (){
+    printf("El programa permite ingresar valores por teclado \n");
+    printf("realiza la suma de los dos valores los muestra por teclado y se despide...\n");
+}
+
+void operacion(){
+    int valor1, valor2, suma;
+    printf("Ingrese el primer valor: ");
+    scanf("%i", &valor1);
+    printf("\n");
+    printf("Ingrese el segundo valor: ");
+    scanf("%i", &valor2);
+    printf("\n");
+    suma = valor1 + valor2;
+    printf("la suma de %i y %i es de %i", valor1, valor2, suma);
+}
+
+void despedida(){
+    printf("\n <--Gracias por utilizar nuestros servicios-->");
+}*/
+
+//-----------------------------------------------------------------
+
+/*
+void lineSeparadora(){
+    printf("_____________________________________");
+}
+void pedirDatosOne(){
+    int i = 0;
+    int valor1, valor2, suma;
+    while(i < 2){
+    printf("\n");
+    printf("Ingrese un valor");
+    scanf("%i", &valor1);
+    printf("\n");
+    printf("Ingrese otro valor");
+    scanf("%i", &valor2);
+    lineSeparadora();
+    suma = valor1 + valor2;
+    printf("\n");
+    printf("%i", suma);
+    printf("\n");
+    lineSeparadora();
+    i++;
+    }
+}
+*/
+
+
+//---------------------------------------------------------------
+
+
+int main(){
+    
+
+    getch();
+    return 0;
+}
+
