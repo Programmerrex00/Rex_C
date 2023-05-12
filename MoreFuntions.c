f30089b082ae1af798e33b92bb6b371ac5ff4e7d
rexmax <maicolcubidespe@gmail.com>
2023-05-12 16:59:30 -0500

    cambios de mierda

---
+++ MoreFuntions.c
@@ -0,0 +1,100 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+/*void cardaDeDatosPorColumna(int matriz[2][5]){
+    for(int c = 0; c < 5 ;c++){
+        for (int f = 0; f < 2; f++){
+            printf("Ingrese el elemento [%i][%i]", f, c);
+            scanf("%i", &matriz[f][c]);
+            printf("\n");
+        }
+    }
+}
+
+void imprimirMatriz(int matriz[2][5]){
+    for (int f = 0; f < 2; f++){
+      for (int c = 0; c < 5; c++){
+        printf("[ %i ]", matriz[f][c]);
+      }
+      printf("\n");
+    }
+}*/
+
+//--------------------------------------------------------------
+/*void CargarElementos(int matriz[3][4]){
+   
+    for (int f = 0; f < 3; f++){
+        for (int c = 0; c < 4; c++){
+            printf("Ingrese el elemento [%i][%i]", f, c);
+            scanf("%i", &matriz[f][c]);
+            printf("\n");
+        }
+    }
+}
+
+int determinarValorMayor(int matriz[3][4]){
+    int elementoMayor = matriz[0][0];
+    for (int f = 0; f < 3; f++){
+        for (int c = 0; c < 4; c++){
+            if(matriz[f][c] > elementoMayor){
+                elementoMayor = matriz[f][c];
+            }
+        }
+        
+    }
+    return elementoMayor;
+    
+}*/
+
+//------------------------------------------------------------------
+
+/*void llenarMatriz(int matriz[2][5]){
+    for (int f = 0; f < 2; f++){
+        for (int c = 0; c < 5; c++){
+            printf("Ingrese el elemento [%i][%i]", f, c);
+            scanf("%i", &matriz[f][c]);
+            printf("\n");
+        }
+    }
+}
+
+void impresion(int matriz[2][5]){
+    for (int f = 0; f < 2; f++){
+        for (int c = 0; c < 5; c++){
+            printf("[ %i ]", matriz[f][c]);
+        }
+        printf("\n");
+    }
+}
+
+void intercambiarFilas(int matriz[2][5]){
+    int auxiliar;
+    for (int f = 0; f < 1; f++){
+        for (int c = 0; c < 5; c++){
+            auxiliar = matriz[f+1][c];
+            matriz[f+1][c] = matriz[f][c];
+            matriz[f][c] = auxiliar;
+        }
+    }
+}*/
+
+//--------------------------------------------------------------
+
+
+int main(){
+    /*int valores[3][4];
+    CargarElementos(valores);
+    printf("El elemento mayor de la matriz Bidimencional es: %i", determinarValorMayor(valores));*/
+   
+   //---------------------------------------------------------------------------------
+    /*int data[2][5];
+    llenarMatriz(data);
+    impresion(data);
+    intercambiarFilas(data);
+    printf("La matriz resultate depues de intercambiar las filas es:\n ");
+    impresion(data);*/
+
+
+}
